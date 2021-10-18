440a07bf2779c9e0409929df8be8a5c10c431b32
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2021-10-18 10:26:58 +0200

    Nornir lab

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 These instructions describe what you need, so that you will be able to run the automation scripts run in your machine.
 
+## Part_1 => Ansible
+
 ### Prerequisites
 
 Packages you need to install:-
@@ -10,16 +12,40 @@ Install python3 and ansible
 
 ```
 sudo apt-get install python3
-pip install ansible ???
+pip install ansible
 ```
 
 
-## Command used to run the playbook
+### Command used to run the playbook
 
 ```
 ansible-playbook -i inventory.ini --vault-id @prompt playbook.yml -K
 vault.yml encryption password => 123abc
 ```
+
+## Part_2 => Nornir
+
+### Prerequisites
+
+Packages you need to install:-
+
+Install nornir using pip
+
+```
+pip install nornir
+pip install nornir-napalm  // Napalm for the connections
+pip install nornir-utils   // 
+pip install nornir_ansible
+```
+
+
+### Command used to run the script
+
+```
+Here comes the way you run the script
+```
+
+
 
 ## Author
 
---
+++ part_nornir/parse.py
@@ -0,0 +1,30 @@
+from nornir import InitNornir
+from ttp import ttp
+from nornir.plugins.tasks import networking
+
+
+def parse_interfaces(config):
+    parser = ttp(data=config, template='ttp/interfaces.j2')
+    parser.parse()
+    interfaces = parser.result()[0][0]
+    return interfaces
+
+def parse_config(task):
+    print('Parsing config for ' + task.host.hostname)
+
+    task.host['interfaces'] = [interface for interface in parse_interfaces(task.host['config'])
+                             if 'mode' in interface.keys()]
+    task.host['access_ports'] = [interface for interface in task.host['interfaces']
+                                      if interface['mode'] == 'access'
+                                      and 'access_vlan' in interface.keys()]
+    task.host['trunk_ports'] = [interface for interface in task.host['interfaces']
+                                     if interface['mode'] == 'trunk'
+                                     and 'native_vlan' in interface.keys()]
+
+
+def get_config(task):
+    r = task.run(
+        task=networking.napalm_get,
+        getters="config"
+    )
+    task.host['config'] = r.result['config']['running']    
