2231ec5c91aac79fd40b830ebe6f2cf569314c1d
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2021-10-21 23:30:05 +0200

    Test some command

--- part_nornir/nornir_auto.py
+++ part_nornir/nornir_auto.py
@@ -15,9 +15,9 @@ ConnectionPluginRegister.available
 nr = InitNornir(config_file="config.yml", dry_run=True)
 
 parsed = nr.run(name="Parse Configurations", task=parse_config)
-print_title(parsed)
-print_result(parsed)
+#print_title(parsed)
+#print_result(parsed)
 
-new_config = nr.run(name="Building New Configs", task=build_config)
-print_result(new_config)
+#new_config = nr.run(name="Building New Configs", task=build_config)
+#print_result(new_config)
 
--- part_nornir/parse.py
+++ part_nornir/parse.py
@@ -14,18 +14,14 @@ def parse_config(task):
     
     task.host['interfaces'] = [interface for interface in parse_interfaces(task.host['config'])
                              if 'mode' in interface.keys()]
-    if task.host.hostname == '172.16.0.13':
-        for i in task.host['interfaces']:
-            if i['interface'] == 'GigabitEthernet0/3':
-                task.host['access_port'] = i['interface']
-    
-    if task.host.hostname == '172.16.0.14':
-        for i in task.host['interfaces']:
-            if i['interface'] == 'GigabitEthernet1/0':
-                task.host['access_port'] = i['interface']
-
+    task.host['access_ports'] = [interface for interface in task.host['interfaces']
+                                      if interface['mode'] == 'access'
+                                      and 'access_vlan' in interface.keys()]
     task.host['trunk_ports'] = [interface for interface in task.host['interfaces']
                                      if interface['mode'] == 'trunk']
+
+    print("Access ports: " + task.host['access_ports'])
+    print("Trunk ports: " + task.host['trunk_ports'])
 
 def get_config(task):
     r = task.run(
