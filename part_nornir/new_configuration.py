10dbf9d65d547b24d21c9e7dee4072e181406eae
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2021-10-21 22:29:39 +0200

    Test some command

---
+++ part_nornir/new_configuration.py
@@ -0,0 +1,26 @@
+from nornir import InitNornir
+from nornir.plugins.tasks import networking, text
+import logging
+
+
+def build_configs(task):
+    r = task.run(task=text.template_file,
+                name="New Configuration",
+                template="vlans.j2",
+                path=f"templates",
+                access_ports=task.host['access_ports'],
+                trunk_ports=task.host['trunk_ports'],
+                severity_level=logging.DEBUG
+                )
+
+    cmds = r.result
+    print(cmds)
+
+    task.host['nconfig'] = r.result
+
+    task.run(task=networking.napalm_configure,
+            name="Loading Configuration on the switch",
+            replace=False,
+            configuration=task.host['nconfig'],
+            severity_level=logging.INFO
+            )
--- part_nornir/parse.py
+++ part_nornir/parse.py
@@ -1,6 +1,7 @@
 from nornir import InitNornir
 from ttp import ttp
 from nornir_napalm.plugins.tasks import napalm_get
+import sys
 
 
 def parse_interfaces(config):
@@ -11,12 +12,16 @@ def parse_interfaces(config):
 
 def parse_config(task):
     get_config(task)
-    
+
     print('Parsing config for ' + task.host.hostname)
     
     task.host['interfaces'] = [interface for interface in parse_interfaces(task.host['config'])
                              if 'mode' in interface.keys()]
-    task.host['access_ports'] = [interface for interface in task.host['interfaces']
+    if task.host.hostname == '172.16.0.13':
+        for i in task.host['interfaces']:
+            print("Interface name is: " + i)
+        sys.die()
+        task.host['access_ports'] = [interface for interface in task.host['interfaces']
                                       if interface['mode'] == 'access'
                                       and 'access_vlan' in interface.keys()]
     task.host['trunk_ports'] = [interface for interface in task.host['interfaces']
