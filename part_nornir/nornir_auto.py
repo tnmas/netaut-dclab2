dda5dde75871def7affdede401c4de6ed3a52659
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2021-10-21 15:02:50 +0200

    Removed unnecessary comment

--- part_nornir/nornir_auto.py
+++ part_nornir/nornir_auto.py
@@ -14,16 +14,8 @@ ConnectionPluginRegister.available
 
 
 nr = InitNornir(config_file="config.yml", dry_run=True)
-#switches = nr.filter(role="switch")
 
-#my_hosts = nr.inventory.hosts
-#host_keys = list(my_hosts.keys())
 result = nr.run(napalm_cli ,commands=["show running-config"])
 
 print_result(result)
-#config = switches.run(name="Get Configurations",task=get_config)
-#print_title(config)
 
-#parsed = switches.run(name="Parse Configurations", task=parse_config)
-#print_title(parsed)
-#print_result(parsed)
