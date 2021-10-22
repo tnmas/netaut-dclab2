5a637f11f404c2de1949840e912bad4157fc7033
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2021-10-22 23:17:40 +0200

    Rename a folder

--- part_nornir/new_configuration.py
+++ part_nornir/new_configuration.py
@@ -11,7 +11,6 @@ def build_config(task):
                 path=f"templates",
                 access_ports=task.host['access_ports'],
                 trunk_ports=task.host['trunk_ports'],
-                switch3_interface= "GigabitEthernet0/3",
                 severity_level=logging.DEBUG
                 )
 
