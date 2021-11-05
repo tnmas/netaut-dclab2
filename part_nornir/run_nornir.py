53f7a109f0ea9bd867b5180e23644a6a2e94f978
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2021-11-05 02:27:47 +0100

    Modify Readme file

--- part_nornir/run_nornir.py
+++ part_nornir/run_nornir.py
@@ -1,11 +1,21 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_title, print_result
-from parse import get_config, parse_config
-from nornir_napalm.plugins.tasks import napalm_get, napalm_cli
-from nornir_netmiko.tasks import netmiko_send_command
+from parse import parse_config
+from new_configs import build_config
+from nornir.core.plugins.connections import ConnectionPluginRegister
+import nornir_napalm.plugins.connections as napalm
+import nornir_netmiko.connections as netmiko
 
+ConnectionPluginRegister.register(napalm.CONNECTION_NAME, napalm.Napalm)
+ConnectionPluginRegister.register(netmiko.CONNECTION_NAME, netmiko.Netmiko)
+ConnectionPluginRegister.available
 
-nr = InitNornir(config_file="config.yaml", dry_run=True)
+nr = InitNornir(config_file="config.yml")
 
-result = nr.run( netmiko_send_command ,command_strings="show running-config")
+parsed = nr.run(name="Parsed Configs.....", task=parse_config)
+print_title(parsed)
+print_result(parsed)
+
+new_config = nr.run(name="New Configs.....", task=build_config)
+print_result(new_config)
 
